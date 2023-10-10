3e21dbcb271bc14447ff456884b5d307810a1bd0
Miguel Santos <130518190+MiguelMfs@users.noreply.github.com>
2023-10-10 18:19:06 -0300

    Update projTorneio.c

--- projTorneio.c
+++ projTorneio.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct _lutador {
+typedef struct _lutador
+{
     char nome[40];
     float altura, peso;
     int idade;
 } Lutador;
 
-typedef struct _torneio {
+typedef struct _torneio
+{
     char nome[40];
     char local[40];
     char categoria[40];
@@ -16,45 +18,53 @@ typedef struct _torneio {
     Lutador *lutadores[100];
 } Torneio;
 
-Lutador *cadastrarLutador(){
+Lutador *cadastrarLutador()
+{
     Lutador *novoLutador = malloc(sizeof(Lutador));
-    printf ("Digite o nome do lutador: ");
-    scanf (" %[^\n]s", novoLutador->nome);
-    printf ("Digite a altura do lutador: ");
+    printf("Digite o nome do lutador: ");
+    scanf(" %[^\n]s", novoLutador->nome);
+    printf("Digite a altura do lutador: ");
     scanf("%f", &novoLutador->altura);
-    printf ("Digite o peso do lutador: ");
+    printf("Digite o peso do lutador: ");
     scanf("%f", &novoLutador->peso);
-    printf ("Digite a idade do lutador: ");
+    printf("Digite a idade do lutador: ");
     scanf("%d", &novoLutador->idade);
-    
+
     return novoLutador;
 }
 
-Torneio *cadastrarTorneio(){
+Torneio *cadastrarTorneio()
+{
     Torneio *novoTorneio = malloc(sizeof(Torneio));
-    printf ("Digite o nome do Torneio: ");
-    scanf (" %[^\n]s", novoTorneio->nome);
-    printf ("Digite o local do Torneio: ");
-    scanf (" %[^\n]s", novoTorneio->local);
-    printf ("Digite a categoria de lutas do Torneio: ");
-    scanf (" %[^\n]s", novoTorneio->categoria);
- 
+    novoTorneio->numLutadores= 0;
+    printf("Digite o nome do Torneio: ");
+    scanf(" %[^\n]s", novoTorneio->nome);
+    printf("Digite o local do Torneio: ");
+    scanf(" %[^\n]s", novoTorneio->local);
+    printf("Digite a categoria de lutas do Torneio: ");
+    scanf(" %[^\n]s", novoTorneio->categoria);
+
     return novoTorneio;
 }
+void editarLutador(Lutador *lutadores[], int numLutadores)
+{
 
-void editarLutador(Lutador *lutadores[], int numLutadores){
-    
-    if(numLutadores == 0) {
+    if (numLutadores == 0)
+    {
         printf("\nNenhum Lutador foi cadastrado para realizar edicoes.");
-    } else {
+    }
+    else
+    {
         char nomeLutador[50];
         int i, opcao;
 
         printf("\nEscreva o nome do Lutador que deseja editar: ");
         scanf(" %[^\n]s", nomeLutador);
 
-        for(i=0; i<numLutadores; i++) {
-            if (strcasecmp(nomeLutador, lutadores[i]->nome) == 0) {
+        for (i = 0; i < numLutadores; i++)
+        {
+            if (strcasecmp(nomeLutador, lutadores[i]->nome) == 0)
+            {
                 printf("\n1 - Para editar o nome do Lutador: ");
                 printf("\n2 - Para editar a altura do Lutador: ");
                 printf("\n3 - Para editar o peso do Lutador: ");
@@ -67,48 +77,53 @@ void editarLutador(Lutador *lutadores[], int numLutadores){
                 case 1:
                     printf("\nDigite o novo nome do Lutador: ");
                     scanf(" %[^\n]s", lutadores[i]->nome);
-                break;
+                    break;
                 case 2:
                     printf("\nDigite a nova altura do Lutador: ");
                     scanf("%f", &lutadores[i]->altura);
-                break;
+                    break;
                 case 3:
                     printf("\nDigite o novo peso do Lutador: ");
                     scanf("%f", &lutadores[i]->peso);
-                break;
+                    break;
                 case 4:
                     printf("\nDigite a nova idade do Lutador: ");
                     scanf("%d", &lutadores[i]->idade);
-                break;
-                
-                
+                    break;
+
                 default:
                     break;
                 }
             }
         }
-    
     }
 }
-void listarLutadores(Lutador *lutadores[], int numLutadores){
+void listarLutadores(Lutador *lutadores[], int numLutadores)
+{
     int i;
-    for(i=0; i<numLutadores; i++){
+    for (i = 0; i < numLutadores; i++)
+    {
         printf(" %s\n", lutadores[i]->nome);
     }
 }
-
-void editarTorneio(Torneio *torneios[], int numTorneios) {
-    if(numTorneios == 0) {
+void editarTorneio(Torneio *torneios[], int numTorneios)
+{
+    if (numTorneios == 0)
+    {
         printf("\nNenhum Torneio foi cadastrado para realizar edicoes.");
-    } else {
+    }
+    else
+    {
         char nomeTorneio[50];
         int i, opcao;
 
         printf("\nEscreva o nome do Torneio que deseja editar: ");
         scanf(" %[^\n]s", nomeTorneio);
 
-        for(i=0; i<numTorneios; i++) {
-            if (strcasecmp(nomeTorneio, torneios[i]->nome) == 0) {
+        for (i = 0; i < numTorneios; i++)
+        {
+            if (strcasecmp(nomeTorneio, torneios[i]->nome) == 0)
+            {
                 printf("\n1 - Para editar o nome do Torneio: ");
                 printf("\n2 - Para editar o local do Torneio: ");
                 printf("\n3 - Para editar a categoria do Torneio: ");
@@ -120,43 +135,139 @@ void editarTorneio(Torneio *torneios[], int numTorneios) {
                 case 1:
                     printf("\nDigite o novo nome do Torneio: ");
                     scanf(" %[^\n]s", torneios[i]->nome);
-                break;
+                    break;
                 case 2:
                     printf("\nDigite o novo local do Torneio: ");
                     scanf(" %[^\n]s", torneios[i]->local);
-                break;
+                    break;
                 case 3:
                     printf("\nDigite a nova categoria de luta do Torneio: ");
                     scanf(" %[^\n]s", torneios[i]->categoria);
-                break;
-                
-                
+                    break;
+
                 default:
                     break;
                 }
             }
         }
-    
     }
 }
-
-void listarTorneios(Torneio *torneios[], int numTorneios) {
+void listarTorneios(Torneio *torneios[], int numTorneios)
+{
     int i;
-    for(i=0; i<numTorneios; i++){
+    for (i = 0; i < numTorneios; i++)
+    {
         printf(" %s\n", torneios[i]->nome);
     }
 }
+void deletarTorneio(Torneio *torneios[], int numTorneios)
+{
+    if (numTorneios == 0)
+    {
+        printf("\nNenhum time foi cadastrado para realizar a remocao\n\n");
+    }
+    else
+    {
+        char nomeTorneio[50];
+        int i, encontrei = 0;
 
-int main(){
+        printf("\nEscreva o nome do Torneio que deseja remover: \n");
+        scanf(" %[^\n]s", nomeTorneio);
+
+        for (i = 0; i < numTorneios; i++)
+        {
+            if (strcasecmp(nomeTorneio, torneios[i]->nome) == 0)
+            {
+                encontrei = 1;
+            }
+            if (encontrei)
+            {
+                if (i < numTorneios - 1) // verifica se existe algo na proxima posicao do vetor
+                    torneios[i] = torneios[i + 1];
+            }
+        }
+        if (encontrei)
+        {
+            printf("\nOperacao realizada com sucesso!\n\n");
+        }
+        else
+        {
+            printf("\nTorneio nao encontrado!\n\n");
+        }
+    }
+}
+
+void adicionarLutadorAoTorneio(Torneio *torneios[], int numTorneios, Lutador *lutadores[], int numLutadores)
+{
+    if (numTorneios == 0 || numLutadores == 0)
+    {
+        printf("\nNenhum Torneio ou Lutador cadastrado para adicionar ao Torneio.\n");
+    }
+    else
+    {
+        char nomeTorneio[50];
+        char nomeLutador[50];
+        int i, j;
+
+        listarTorneios(torneios, numTorneios);
+
+        printf("\nEscreva o nome do Torneio ao qual deseja adicionar um Lutador: ");
+        scanf(" %[^\n]s", nomeTorneio);
+
+        for (i = 0; i < numTorneios; i++)
+        {
+            if (strcasecmp(nomeTorneio, torneios[i]->nome) == 0)
+            {
+                listarLutadores(lutadores, numLutadores);
+                printf("\nEscreva o nome do Lutador que deseja adicionar ao Torneio: ");
+                scanf(" %[^\n]s", nomeLutador);
+
+                for (j = 0; j < numLutadores; j++)
+                {
+                    if (strcasecmp(nomeLutador, lutadores[j]->nome) == 0)
+                    {
+                        // Adicionar o lutador ao torneio
+                        if (torneios[i]->numLutadores < 100)
+                        {
+                            torneios[i]->lutadores[torneios[i]->numLutadores] = lutadores[j];
+                            torneios[i]->numLutadores++;
+                            printf("\nLutador %s adicionado ao Torneio %s com sucesso!\n", lutadores[j]->nome, torneios[i]->nome);
+                        }
+                        else
+                        {
+                            printf("\nO Torneio atingiu o limite de lutadores.\n");
+                        }
+                        break; // Não é necessário continuar procurando após encontrar o lutador
+                    }
+                }
+
+                if (j == numLutadores)
+                {
+                    printf("\nLutador não encontrado.\n");
+                }
+
+                break; // Não é necessário continuar procurando após encontrar o torneio
+            }
+        }
+
+        if (i == numTorneios)
+        {
+            printf("\nTorneio não encontrado.\n");
+        }
+    }
+}
+
+int main()
+{
     Lutador *lutadores[100];
     Torneio *torneios[100];
-    int opcao; 
+    int opcao, opcaoLutador;
     int numTorneios = 0;
     int numLutadores = 0;
-    char sair;
-    
+    char sair, reiniciar;
 
-    do {
+    do
+    {
         printf("\n\n\tMENU\n");
         printf("\n1 - Cadastrar um novo Lutador:");
         printf("\n2 - Editar uma caracteristica do Lutador:");
@@ -170,97 +281,106 @@ int main(){
         scanf("%d", &opcao);
         printf("\n\n");
 
-        switch (opcao){
-        case 1: 
-        lutadores[numLutadores] = cadastrarLutador();
-        numLutadores++;
+        switch (opcao)
+        {
+        case 1:
+            lutadores[numLutadores] = cadastrarLutador();
+            numLutadores++;
             break;
-        case 2: 
-        listarLutadores(lutadores, numLutadores);
-        editarLutador(lutadores, numLutadores);
+        case 2:
+            listarLutadores(lutadores, numLutadores);
+            editarLutador(lutadores, numLutadores);
             break;
-        case 3: 
-        listarLutadores(lutadores, numLutadores);
+        case 3:
+            listarLutadores(lutadores, numLutadores);
             break;
-        case 4: 
-        torneios[numTorneios] = cadastrarTorneio();
-        numTorneios++;
+        case 4:
+            torneios[numTorneios] = cadastrarTorneio();
+            numTorneios++;
             break;
-        case 5: 
-        listarTorneios(torneios, numTorneios);
+        case 5:
+            listarTorneios(torneios, numTorneios);
             break;
         case 6:
-            if(numTorneios == 0) {
+            if (numTorneios == 0)
+            {
                 printf("\nNenhum Torneio foi cadastrado para realizar edicoes.");
-            } else {
-            
-                char nomeTorneio[50];
-                int i, opcao, opcaoJogador;
-        
-                listarTorneios(torneios, numTorneios);
-        
-                printf("\nEscreva o nome do Torneio que deseja editar: ");
-                scanf(" %[^\n]s", nomeTorneio);
-
-                for(i=0; i<numTorneios; i++) {
-                    if (strcasecmp(nomeTorneio, torneios[i]->nome) == 0) {
-                        printf("\n1 - Para editar o nome do Torneio: ");
-                        printf("\n2 - Para adicionar um Lutador ao Torneio: ");
-                        printf("\n3 - Para remover um lutador do Torneio: ");
-                        printf("\n\nSelecione uma opcao: ");
-                        scanf("%d", &opcao);
-
-                switch (opcao){
-                case 1:
-                    printf("\nDigite o novo nome do Torneio: ");
-                    scanf(" %[^\n]s", torneios[i]->nome);
-                break;
-                case 2:
-                    if (torneios[i]->numLutadores >= 100) {
-                        printf("\nO Torneio ja possui o numero maximo de lutadores.");
-                } else {
-                    int j;
-                    listarLutadores(lutadores, numLutadores);
-                    printf("\nEscolha o lutador a ser adicionado: ");
-
-                    if (strcasecmp(nomeTorneio, torneios[i]->nome) == 0) {
-                        Lutador *lutadorSelecionado = lutadores[opcao - 1];
-                        torneios[i]->lutadores[torneios[i]->numLutadores] = lutadorSelecionado;
-                        torneios[i]->numLutadores++;
-                         printf("%s adicionado ao Torneio %s.\n", lutadorSelecionado->nome, torneios[i]->nome);
-                    } else {
-                    printf("\nLutador invalido. Escolha um lutador valido.");
-                    }
-                }
-                break;
-                case 3:
+            }
+            else
+            {
                 
-                break;
-                }
-            
-            }  
-        }      
-    }    
 
+                        do
+                        {
+                            printf("\n1 - Para editar o nome do Torneio: ");
+                            printf("\n2 - Para adicionar um Lutador ao Torneio: ");
+                            printf("\n3 - Para remover um lutador do Torneio: ");
+                            printf("\n4 - voltar ao menu principal ");
+                            printf("\n\nSelecione uma opcao: ");
+                            scanf("%d", &opcaoLutador);
+
+                            switch (opcaoLutador)
+                            {
+                            case 1:
+                            char nomeTorneio[50];
+                            int i;
+
+                            listarTorneios(torneios, numTorneios);
+
+                            printf("\nEscreva o nome do Torneio que deseja editar: ");
+                            scanf(" %[^\n]s", nomeTorneio);
+                            for (i = 0; i < numTorneios; i++) {
+                                if (strcasecmp(nomeTorneio, torneios[i]->nome) == 0)
+                                {
+                                printf("\nDigite o novo nome do Torneio: ");
+                                scanf(" %[^\n]s", torneios[i]->nome);
+                                }
+                            }
+                                break;
+                            case 2:
+                                adicionarLutadorAoTorneio(torneios, numTorneios, lutadores, numLutadores);
+                                break;
+                            case 3:
+
+                                break;
+                            case 4:
+
+                                printf("Deseja voltar ao menu principal? (S/N): ");
+                                scanf(" %c", &reiniciar);
+                                break;
+
+                            default:
+                                printf("\nOpcao inexistente!\n\n");
+                            }
+
+                            if (opcaoLutador != 4)
+                            {
+                                reiniciar = 'n';
+                            }
+                        } while (reiniciar == 'N' || reiniciar == 'n');
+            }
             break;
         case 7:
+            listarTorneios(torneios, numTorneios);
+            deletarTorneio(torneios, numTorneios);
+            numTorneios--;
             break;
-        case 8: 
+        case 8:
             printf("Deseja sair? (S/N): ");
             scanf(" %c", &sair);
             break;
 
-        default:    
+        default:
             puts("Opcao Invalida!");
             break;
         }
 
-        if (opcao != 8) {
+        if (opcao != 8)
+        {
             sair = 'n';
         }
 
-
     } while (sair == 'N' || sair == 'n');
-    
+
     return 0;
 }
